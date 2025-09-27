3b81804ab9c239864dcb6dfe0ae7dfd888d75b53
unknown <gwyneth.maiso@evsu.edu.ph>
2025-09-27 17:30:48 +0800

    Week1: Initial Django scafold (pages)

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    ctx = {
+        'course': 'IT373 - Web Systems and Technologies 2',
+        'week': 1,
+    }
+    return render(request, 'pages/home.html', ctx)
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def hello(request):
+    return render(request, 'pages/hello.html')
